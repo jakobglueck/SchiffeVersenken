fb367044c8f9e00817b1281673d3f0a97d72cc2e
Erwin Bause <erwin.bause@stud.htwk-leipzig.de>
2024-07-30 22:13:45 +0200

    ADD - create test label for each board

--- src/View/BoardView.java
+++ src/View/BoardView.java
@@ -1,9 +1,17 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class BoardView extends JFrame {
+public class BoardView extends JPanel {
     public BoardView(){
-        
+        JLabel label = new JLabel("Hallo");
+        this.add(label);
+        this.setVisible(true);
+        generateBoard();
+    }
+    public void generateBoard() {
+        this.setLayout(new GridLayout(10, 10));
+        //TODO
     }
 }
