90d64845bbea6d92695fdee5664006b45fd91b43
Erwin Bause <erwin.bause@stud.htwk-leipzig.de>
2024-07-30 20:54:59 +0200

    FIX - fixed GameView constructor and imports

--- src/SchiffeVersenkenMain.java
+++ src/SchiffeVersenkenMain.java
@@ -1,10 +1,11 @@
+import View.*;
 public class SchiffeVersenkenMain{
     public static void main(String[] args){
 
         //Model - TODO
 
         //View
-        GameView gameView = GameView();
+        GameView gameView = new GameView();
 
         //Controller - TODO
 
