c730f6c86e07fb769635eb0aed2eb95fb7c240a2
Erwin Bause <erwin.bause@stud.htwk-leipzig.de>
2024-07-30 22:13:13 +0200

    ADD - added empty playerboards to main-window

--- src/View/GameView.java
+++ src/View/GameView.java
@@ -1,12 +1,25 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameView extends JFrame {
+    private BoardView playerBoardOne;
+    private BoardView playerBoardTwo;
     public GameView(){
-        this.setTitle("Schiffe versenken");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(800, 600);
+        setTitle("Schiffe versenken");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+
+        setLayout(new GridLayout(2, 2));
+
+        this.playerBoardOne = new BoardView();
+        this.playerBoardTwo = new BoardView();
+
+        add(playerBoardOne);
+        add(playerBoardTwo);
+
+
         this.setVisible(true);
     }
 }
