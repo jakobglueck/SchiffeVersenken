288c65066bbebbe6bfdb03decbace135247f2ecc
Erwin Bause <erwin.bause@stud.htwk-leipzig.de>
2024-07-30 15:48:30 +0200

    UPDATE - SchiffeVersenkenMain.java

--- src/SchiffeVersenkenMain.java
+++ src/SchiffeVersenkenMain.java
@@ -1,5 +1,6 @@
-public class SchiffeVersenkenMain {
+public class SchiffeVersenkenMain{
     public static void main(String[] args){
         System.out.println("Ausgabe aus der main()-Methode");
     }
 }
+
