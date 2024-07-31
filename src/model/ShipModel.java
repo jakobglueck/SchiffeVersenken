235d8b6ad77cef17ce096378845c18546230d4a4
Jakob Glück <jakob.glueck4@gmail.com>
2024-07-31 17:10:21 +0200

    ADD - Ship class  attributes and main Functions

--- src/model/ShipModel.java
+++ src/model/ShipModel.java
@@ -1,2 +1,40 @@
-package model;public class ShipModel {
+package model;
+
+import model.CellModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShipModel {
+
+    private ArrayList<CellModel> shipCells;
+
+    public ShipModel(){
+    }
+
+    public List<CellModel> getShip(){
+        return shipCells;
+    }
+    public void setShip(ArrayList<CellModel> ship){
+        this.shipCells = ship;
+    }
+
+    public void  addShipCells(CellModel cell){
+        this.shipCells.add(cell);
+    }
+
+    public void removeShipCells(CellModel cell){
+        this.shipCells.remove(cell);
+    }
+
+    public void removeAllShipCells(){
+        this.shipCells.clear();
+    }
+
+    public boolean checkShipLength(){
+        if(this.shipCells.size()>4 && this.shipCells.size() < 2){
+            return false;
+        }
+        return true;
+    }
 }
