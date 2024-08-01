da166308942759a74cb2063800d9799312211af2
Erwin Bause <erwin.bause@stud.htwk-leipzig.de>
2024-08-01 16:29:42 +0200

    UPDATE - new object of BoardModel

--- src/SchiffeVersenkenMain.java
+++ src/SchiffeVersenkenMain.java
@@ -1,11 +1,13 @@
 import View.*;
+import model.BoardModel;
+
 public class SchiffeVersenkenMain{
     public static void main(String[] args){
 
         //Model - TODO
-
+        BoardModel bm = new BoardModel();
         //View
-        GameView gameView = new GameView();
+        GameView gameView = new GameView(bm);
 
         //Controller - TODO
 
