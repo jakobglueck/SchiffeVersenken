b78b35f325dfab43bde4ceacd6bd64c2d1c41462
Erwin Bause <erwin.bause@stud.htwk-leipzig.de>
2024-08-02 11:04:45 +0200

    ADD - playerNamePanel

--- src/View/GameView.java
+++ src/View/GameView.java
@@ -12,7 +12,7 @@ public class GameView extends JFrame {
     private PlayerModel playerTwo;
     private BoardView playerBoardOne;
     private BoardView playerBoardTwo;
-    private BoardModel boardModel;
+    private GameModel gameModel;
 
     public GameView(GameModel gm) {
         this.gameModel = gm;
@@ -21,23 +21,23 @@ public class GameView extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
 
-
         this.playerOne = gm.getPlayerOne();
         this.playerTwo = gm.getPlayerTwo();
-
 
         this.playerBoardOne = new BoardView(playerOne.getBoard());
         this.playerBoardTwo = new BoardView(playerTwo.getBoard());
 
-        // bm.randomlyPlaceShips(); // Zufällige Platzierung der Schiffe auf dem ersten Spielbrett
         playerBoardOne.updateBoard(); // Aktualisiere das Spielbrett
 
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
-        // Labels für Spieler 1 und Spieler 2
-        addComponent(new JLabel(this.playerOne.getPlayerName(), JLabel.CENTER), gbc, 0, 0, 1, 1, 10, 0.0, 0.0, GridBagConstraints.NONE);
-        addComponent(new JLabel(this.playerTwo.getPlayerName(), JLabel.CENTER), gbc, 2, 0, 1, 1, 10, 0.0, 0.0, GridBagConstraints.NONE);
+        // PlayerNamePanels für Spieler 1 und Spieler 2
+        JPanel playerNamePanelOne = createPlayerNamePanel(this.playerOne.getPlayerName());
+        JPanel playerNamePanelTwo = createPlayerNamePanel(this.playerTwo.getPlayerName());
+
+        addComponent(playerNamePanelOne, gbc, 0, 0, 1, 1, 10, 0.0, 0.0, GridBagConstraints.NONE);
+        addComponent(playerNamePanelTwo, gbc, 2, 0, 1, 1, 10, 0.0, 0.0, GridBagConstraints.NONE);
 
         // BoardView für Spieler 1 und Spieler 2
         addComponent(playerBoardOne, gbc, 0, 1, 1, 1, 10, 0.5, 0.5, GridBagConstraints.BOTH);
@@ -50,6 +50,13 @@ public class GameView extends JFrame {
         addComponent(controlPanel, gbc, 0, 2, 3, 1, 10, 1.0, 0.0, GridBagConstraints.HORIZONTAL);
 
         setVisible(true);
+    }
+
+    private JPanel createPlayerNamePanel(String playerName) {
+        JPanel playerNamePanel = new JPanel();
+        JLabel playerNameLabel = new JLabel(playerName, SwingConstants.CENTER);
+        playerNamePanel.add(playerNameLabel);
+        return playerNamePanel;
     }
 
     private void addControlButtons(JPanel controlPanel) {
@@ -67,24 +74,13 @@ public class GameView extends JFrame {
         pauseGameButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Implementieren Sie hier die Aktion zum Pausieren des Spiels
                 JOptionPane.showMessageDialog(null, "Spiel ist pausiert!");
             }
         });
 
-        /*
-        mainMenuButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                startNewGame();
-            }
-        });
-        */
-
         controlPanel.add(mainMenuButton);
         controlPanel.add(pauseGameButton);
         controlPanel.add(endGameButton);
-
     }
 
     private void addComponent(Component component, GridBagConstraints gbc, int gridx, int gridy, int gridwidth, int gridheight, int insets, double weightx, double weighty, int fill) {
