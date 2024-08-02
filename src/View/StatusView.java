d01b5f1366f387355c675e021b318b68914f8e50
Erwin Bause <erwin.bause@stud.htwk-leipzig.de>
2024-08-02 12:02:32 +0200

    ADD - new panel, which shows status

--- src/View/StatusView.java
+++ src/View/StatusView.java
@@ -1,2 +1,21 @@
-package View;public class StatusView {
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StatusView extends JPanel {
+    private JLabel statusLabel;
+
+    public StatusView() {
+        this.setLayout(new BorderLayout());
+        statusLabel = new JLabel("Status: Warten auf Spielbeginn", SwingConstants.CENTER);
+        this.add(statusLabel, BorderLayout.CENTER);
+
+        // Setze Padding
+        this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    }
+
+    public void updateStatus(String status) {
+        statusLabel.setText("Status: " + status);
+    }
 }
