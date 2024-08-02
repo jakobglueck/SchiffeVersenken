3f809b761fc19c92cbf1be37ab9625211e58227c
Erwin Bause <erwin.bause@stud.htwk-leipzig.de>
2024-08-02 11:12:10 +0200

    ADD - Outsourcing the PlayerView to a separate class

--- src/View/PlayerView.java
+++ src/View/PlayerView.java
@@ -1,2 +1,18 @@
-package View;public class PlayerView {
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayerView extends JPanel {
+    private JLabel playerNameLabel;
+
+    public PlayerView(String playerName) {
+        this.setLayout(new BorderLayout());
+        playerNameLabel = new JLabel(playerName, SwingConstants.CENTER);
+        this.add(playerNameLabel, BorderLayout.CENTER);
+    }
+
+    public void setPlayerName(String playerName) {
+        playerNameLabel.setText(playerName);
+    }
 }
