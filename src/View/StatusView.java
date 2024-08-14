907a388946fe95b3e9cc6c1159e2444904ded1d3
Erwin Bause <erwin.bause@stud.htwk-leipzig.de>
2024-08-14 19:04:48 +0200

    UPDATE - create new label for more game infos

--- src/View/StatusView.java
+++ src/View/StatusView.java
@@ -5,27 +5,37 @@ import java.awt.*;
 
 public class StatusView extends JPanel {
     private JLabel titleLabel;
-    private JLabel statusLabel;
+    private JLabel playerNameLabel;
+    private JLabel additionalInfoLabel;
 
     public StatusView() {
-        this.setLayout(new GridLayout(2, 1,0,-75));
+        this.setLayout(new GridLayout(3, 1, 0, -75));
 
         titleLabel = new JLabel("Status", SwingConstants.CENTER);
-        statusLabel = new JLabel("Warten auf Spielbeginn", SwingConstants.CENTER);
+        playerNameLabel = new JLabel("", SwingConstants.CENTER);
+        additionalInfoLabel = new JLabel("", SwingConstants.CENTER);
 
         this.add(titleLabel);
-        this.add(statusLabel);
-        
+        this.add(playerNameLabel);
+        this.add(additionalInfoLabel);
+
         this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
     }
 
-    public void updateStatus(String status) {
-        statusLabel.setText(status);
-        statusLabel.revalidate();
-        statusLabel.repaint();
+    public void updatePlayerName(String status) {
+        playerNameLabel.setText(status);
+        playerNameLabel.revalidate();
+        playerNameLabel.repaint();
+    }
+
+    public void updateAdditionalInfo(String info) {
+        additionalInfoLabel.setText(info); // Text des neuen Labels aktualisieren
+        additionalInfoLabel.revalidate();
+        additionalInfoLabel.repaint();
     }
 
     public void reset() {
-        updateStatus("Warten auf Spielbeginn");
+        updatePlayerName("Warten auf Spielbeginn");
+        updateAdditionalInfo(""); // Zusätzliche Infos zurücksetzen
     }
 }
